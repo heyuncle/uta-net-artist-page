3776592943a999300f645a02e2eeda462eb60b01
heyuncle <heyuncle543@gmail.com>
2023-06-07 16:00:15 -0400

    remove nonfunctional line

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from artist_page import gen_artist_html
 def main():
     t = MeCab.Tagger()
     try:
-        id = int(sys.argv[1])
         gen_artist_html(sys.argv[1], t) # artist_id in url on uta-net.com
     except:
         print("Failed to get songs for artist")
